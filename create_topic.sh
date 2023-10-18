71510c8cfacd8b540fb279cd721b291c1ba50ed7
Максимилиан Мамиляев <maksimilianmamilaev@Maksimilians-MacBook-Pro.local>
2023-10-18 10:53:22 +0200

    Added topic info

--- create_topic.sh
+++ create_topic.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-kafka-topics --create --topic subsCount --bootstrap-server broker:9092 --replication-factor 1 --partitions 1
-kafka-topics --create --topic postContent --bootstrap-server broker:9092 --replication-factor 1 --partitions 1
-kafka-topics --create --topic postStat --bootstrap-server broker:9092 --replication-factor 1 --partitions 1
+kafka-topics --create --topic subsCount --bootstrap-server broker:9092 --replication-factor 3 --partitions 1
+kafka-topics --create --topic channelMeta --bootstrap-server broker:9092 --replication-factor 3 --partitions 1
+kafka-topics --create --topic postContent --bootstrap-server broker:9092 --replication-factor 3 --partitions 1
+kafka-topics --create --topic postStat --bootstrap-server broker:9092 --replication-factor 3 --partitions 1
